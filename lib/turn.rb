e0075220bd8954c22350f0538bdc9200af0e85e2
Brendan O'Connell <bmoconnell15@gmail.com>
2020-09-25 14:19:59 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i
+  index = index - 1 
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+  display_board(board)
+end
